0d46042ac82b3b0c3b977950558ef41e3a4bea79
Mutsanna <am.qoid@gmail.com>
2020-03-12 14:04:13 +0700

    coomite pertama

---
+++ js/template.js
@@ -0,0 +1,9 @@
+var vm = new Vue({
+    el: '#vue_det',
+    data: {
+        firstname: "Satria",
+        lastname: "Dermawan",
+        htmlcontent: "<div>Vue js tamplate</div>",
+        imgsrc: "images/angry-regular.svg"
+    }
+})
