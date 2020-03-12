ca21da84b1713383bbbea9cf6a3d46f1d0b104d3
Mutsanna <am.qoid@gmail.com>
2020-03-12 15:38:55 +0700

    Hapus file double, dan nambah vue watch

--- belajar_vue/js/template.js
+++
@@ -1,9 +0,0 @@
-var vm = new Vue({
-    el: '#vue_det',
-    data: {
-        firstname: "Satria",
-        lastname: "Dermawan",
-        htmlcontent: "<div>Vue js tamplate</div>",
-        imgsrc: "images/angry-regular.svg"
-    }
-})
